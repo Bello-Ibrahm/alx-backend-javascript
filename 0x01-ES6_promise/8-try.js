45adba517ecb031a51c637c7c70d0a73071fbed6
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-10 14:03:28 +0000

    Fix checker requirment #3

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('Cannot divide by zero');
-    }
-    return (numerator / denominator);
-  } catch (error) {
-    return error.message; 
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
   }
+  return numerator / denominator;
 }
-
