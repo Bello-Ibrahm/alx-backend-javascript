c0495b304c47c15b7977e365d77bd24fcb54be10
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-11 13:30:02 +0000

    Task 4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(amnt) {
+    this._amount = amnt;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(curr) {
+    this._currency = curr;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static converPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
