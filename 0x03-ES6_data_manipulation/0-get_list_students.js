c1bf2a30f908dc7be139e0167dc41a1af094a9bb
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-15 20:46:24 +0000

    Fix checker output requirement

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
-const getListStudents = () => [
-  { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstname: 'James', location: 'Columbia' },
-  { id: 5, firstname: 'Serena', location: 'San Francisco' },
-];
+const getListStudents = () => {
+  const students = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+};
 
 export default getListStudents;
