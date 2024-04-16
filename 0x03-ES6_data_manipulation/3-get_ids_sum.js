a12322a1cdb4d3e73be75a14ef597b7259d63205
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-16 08:25:40 +0000

    Add Docblock

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,16 @@
 /**
- * Calculates the sum of the id properties from an array of objects (arrList)
- * (acc, value) => acc + value.id, 0:
- *  acc is initialized with 0 + current value in the iteration.
+ * Calculates the sum of student IDs from an array of objects.
  *
- * @returns {Number} - the final sum of the id properties (accumulator).
+ * The function uses the `reduce` method to iterate through an array of objects (`arrList`) and
+ * calculates the sum of the `id` properties.
+ * The initial value of the accumulator is set to 0.
+ * Each object in the array is expected to have an `id` property, which is a numeric value.
+ *
+ * @param {Array} arrList - An array of objects representing students.
+ * Each object in the array is expected to have an `id` property, which is a numeric value.
+ *
+ * @returns {number} - The sum of the `id` properties from the array of objects.
+ * Returns 0 if the array is empty or if none of the objects have an `id` property.
  */
 const getStudentIdsSum = (arrList) => {
   const idsSum = arrList.reduce(
