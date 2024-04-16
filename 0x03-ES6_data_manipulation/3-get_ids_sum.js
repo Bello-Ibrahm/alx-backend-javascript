13d1499a2518fb1c1f04feb3efd01c868899dd12
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-16 08:19:09 +0000

    Add blockquotes

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates the sum of the id properties from an array of objects (arrList)
+ * (acc, value) => acc + value.id, 0:
+ *  acc is initialized with 0 + current value in the iteration.
+ *
+ * @returns {Number} - the final sum of the id properties (accumulator).
+ */
 const getStudentIdsSum = (arrList) => {
   const idsSum = arrList.reduce(
     (acc, value) => acc + value.id, 0,
