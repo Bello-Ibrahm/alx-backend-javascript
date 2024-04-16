9e0799b52992322aeabcd2ef5156c015049f72d1
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-16 10:43:51 +0000

    Change code logic and add Docblock

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,19 @@
-const getStudentsByLocation = (arrObjList, city) => {
-  const arrObjFilters = arrObjList.filter((item) => item.location === city);
-  return arrObjFilters;
-};
-
-export default getStudentsByLocation;
+/**
+ * Filters an array of student objects by location.
+ *
+ * This function takes an array of student objects (`student`) and a city name (`city`),
+ * and returns a new array containing only the students who are located in the specified city.
+ * Each student object in the input array is expected to have a `location` property,
+ * which is a string.
+ *
+ * @param {Array} student - An array of student objects.
+ * Each student object is expected to have a `location` property, which is a string.
+ *
+ * @param {string} city - The name of the city to filter the students by.
+ *
+ * @returns {Array} - An array of student objects located in the specified city.
+ * If no students are located in the specified city, an empty array is returned.
+ */
+export default function getStudentsByLocation(student, city) {
+  return student.filter((item) => item.location === city);
+}
